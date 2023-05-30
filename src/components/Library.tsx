299e20b72184f889ed08000c7048363f5c36511c
Devon McGrath <devon.mcgrath@gmail.com>
2023-05-31 10:37:14 +1200

    Add Book constructor in typescript

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -1,5 +1,30 @@
 import * as React from "react";
 
+let myLibrary = [];
+
+class Book {
+  title: string;
+  author: string;
+  pages: number;
+  readStatus: string;
+
+  constructor(
+    theTitle: string,
+    theAuthor: string,
+    pageCount: number,
+    theReadStatus: string
+  ) {
+    this.title = theTitle;
+    this.author = theAuthor;
+    this.pages = pageCount;
+    this.readStatus = theReadStatus;
+  }
+}
+
+let harryPotter = new Book("Harry Potter", "J.K Rowling", 300, "Read");
+
+function addBookToLibrary() {}
+
 const Library = () => {
   return (
     <div>
