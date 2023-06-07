1ed6742ba546763f0983c22827da2d553720672d
Devon McGrath <devon.mcgrath@gmail.com>
2023-06-07 16:10:24 +1200

    Add modal to button

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import FormModal from "./FormModal";
 import {
   contentWrapper,
   grid,
@@ -58,6 +57,7 @@ const Library = () => {
     setTitleText(INITIAL_TITLE);
     setAuthorName(INITIAL_AUTHOR);
     setChecked(INITIAL_CHECKBOX);
+    setOpenModal(!openModal);
   }
 
   // book modal (if statement?? )
@@ -65,7 +65,6 @@ const Library = () => {
   const libraryItems = myLibrary.map((book) => (
     <div className={card}>
       <div className={cardContent}>
-        <h3>{book.id}</h3>
         <h3>{book.title}</h3>
         <h4>{book.author}</h4>
         <p>{book.readStatus}</p>
@@ -80,39 +79,41 @@ const Library = () => {
           Add Book
         </button>
       </div>
-      {openModal ? <FormModal></FormModal> : null}
-      <div className={formModal}>
-        <form onSubmit={addBookToLibrary} className={form}>
-          <label htmlFor="title">Add Book:</label>
-          <input
-            required
-            type="text"
-            id="title"
-            value={titleText}
-            placeholder="Title"
-            onChange={(e) => setTitleText(e.target.value)}
-          />
-          <input
-            required
-            type="text"
-            id="author"
-            value={authorName}
-            placeholder="Author"
-            onChange={(e) => setAuthorName(e.target.value)}
-          />
-          <div className={readStatus}>
-            <label htmlFor="readStatus">Have you read it?</label>
-            <input
-              type="checkbox"
-              id="readStatus"
-              value={checked}
-              onChange={handleChange}
-            />
+      {openModal ? (
+        <div className={modalOverlay}>
+          <div className={formModal}>
+            <form onSubmit={addBookToLibrary} className={form}>
+              <label htmlFor="title">Add Book:</label>
+              <input
+                required
+                type="text"
+                id="title"
+                value={titleText}
+                placeholder="Title"
+                onChange={(e) => setTitleText(e.target.value)}
+              />
+              <input
+                required
+                type="text"
+                id="author"
+                value={authorName}
+                placeholder="Author"
+                onChange={(e) => setAuthorName(e.target.value)}
+              />
+              <div className={readStatus}>
+                <label htmlFor="readStatus">Have you read it?</label>
+                <input
+                  type="checkbox"
+                  id="readStatus"
+                  value={checked}
+                  onChange={handleChange}
+                />
+              </div>
+              <button onClick={addBookToLibrary}>Add Book</button>
+            </form>
           </div>
-          <button onClick={addBookToLibrary}>Add Book</button>
-        </form>
-      </div>
-      <div className={modalOverlay}></div>
+        </div>
+      ) : null}
       <div className={grid}>{libraryItems}</div>
     </div>
   );
